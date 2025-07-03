9d2c6f29624114b1dd81dccfb835fb244ae125c5
zhf2680536614 <zhf19993612440@outlook.com>
2025-07-03 20:02:10 +0800

    RBAC实体类创建优化

--- service/src/main/java/com/atey/mapper/UserRoleMapper.java
+++ service/src/main/java/com/atey/mapper/UserRoleMapper.java
@@ -2,6 +2,7 @@ package com.atey.mapper;
 
 import com.atey.entity.UserRole;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author atey
  * @since 2025-07-03
  */
+@Mapper
 public interface UserRoleMapper extends BaseMapper<UserRole> {
 
 }
